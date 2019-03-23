e52ae2464b393c7208b3f7f0ab92602c2413b895
jacob guenther <jacobrwguenther@gmail.com>
2019-03-22 17:44:20 -0800

    skeleton of CompoundShape class.

--- CPS/compoundshape.cpp
+++ CPS/compoundshape.cpp
@@ -2,3 +2,20 @@
 // Created by Mark, Bryant and Jacob on 3/20/2019.
 //
 
+#include "compoundshape.h"
+
+CompoundShape::CompoundShape(const vector<Shape*> &shapes)
+	: _shapes = shapes
+{}
+
+int CompoundShape::get_width() const
+{
+	// auto total = 0;
+	// for_each(_shapes.begin(), _shapes.end(),
+	// 	[total](Shape shape){total += shape->get_width()});
+	// return total;
+}
+int CompoundShape::get_height() const
+{
+
+}
--- CPS/compoundshape.h
+++ CPS/compoundshape.h
@@ -5,4 +5,30 @@
 #ifndef CS372_CPS_COUMPOUNDSHAPE_H
 #define CS372_CPS_COUMPOUNDSHAPE_H
 
+#include <algorithm>
+using std::for_each;
+#include <vector>;
+using std::vector;
+#include "shape.h"
+
+class CompoundShape: public Shape {
+public:
+	CompoundShape(const vector<Shape*> &shapes);
+	virtual ~CompoundShape()=default;
+
+	int get_width() const override;
+	int get_height() const override;
+	void set_width(int width);
+	void set_height(int width);
+private:
+	vector<Shape*> _shapes;
+};
+
+class VirticalShapes: public CompoundShape {
+public:
+
+private:
+
+};
+
 #endif //CS372_CPS_COUMPOUNDSHAPE_H
