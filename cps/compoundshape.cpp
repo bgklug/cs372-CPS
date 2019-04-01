b569e3b469921176d1a8cfde74d8d718139deed5
jacob guenther <jacobrwguenther@gmail.com>
2019-03-31 19:38:03 -0800

    Move Rotate from compoundshape to shape

--- cps/compoundshape.cpp
+++ cps/compoundshape.cpp
@@ -126,31 +126,6 @@ stringstream VerticalShapes::generate()
 	return postScriptFragment;
 }
 
-Rotated::Rotated(std::unique_ptr<Shape> shape, int degrees) : _rotation{degrees}
-{
-    if(degrees == 90 || degrees == 270)
-    {
-        set_height(shape->get_width());
-        set_width(shape->get_height());
-    }
-    else
-    {
-        set_width(shape->get_width());
-        set_height(shape->get_height());
-    }
-
-    _originalShape = std::move(shape);
-}
-
-std::stringstream Rotated::generate()
-{
-    return std::stringstream ("gsave\n"
-                              + std::to_string(_rotation) + " rotate\n"
-                              + _originalShape->generate().str()
-                              + "grestore\n");
-}
-
-
 HorizontalShapes::HorizontalShapes(std::vector<Shape_ptr> shapes)
 	: CompoundShape(move(shapes))
 {}
