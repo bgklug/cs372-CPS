138fd68bfc613dd82d0d112f7bfb1c174c704f7b
jacob guenther <jacobrwguenther@gmail.com>
2019-04-01 17:15:40 -0800

    Add Scaled examples.

--- docs/examples/example.cpp
+++ docs/examples/example.cpp
@@ -8,6 +8,8 @@ using std::vector;
 using std::make_unique;
 using std::move;
 
+#include <iostream>
+
 #include "../../cps/cps.hpp"
 using namespace cps;
 
@@ -35,6 +37,20 @@ int main() {
         file << Spacer(INCH, 0).generate().str();
         file << column.generate().str();
     }
+
+    file << Spacer(-1*INCH, -2*INCH).generate().str();
+
+    Circle c1(0.5*INCH);
+    file << Scaled(c1, {1, 1}).generate().str();
+    file << Scaled(c1, {2, 1}).generate().str();
+    file << Scaled(c1, {4, 1}).generate().str();
+ 
+    file << Spacer(-1*INCH, 6*INCH).generate().str();
+
+    HorizontalShapes horizontal;
+    horizontal.pushShape(make_unique<Square>(INCH));
+    horizontal.pushShape(make_unique<Circle>(INCH));
+    file << Scaled(horizontal, {2, 1}).generate().str();
 
     file << SHOWPAGE;
 
