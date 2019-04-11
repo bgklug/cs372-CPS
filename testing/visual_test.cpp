b8f5604b737965310a32fe12b963348218fb6f3d
Bryant <bgklug@alaska.edu>
2019-04-10 19:35:35 -0800

    Fix Polygon centering

---
+++ testing/visual_test.cpp
@@ -0,0 +1,45 @@
+//
+// Created by bryant on 4/10/19.
+//
+
+
+#include <fstream>
+using std::fstream;
+#include <vector>
+using std::vector;
+#include <memory>
+using std::make_unique;
+using std::move;
+
+#include <iostream>
+
+#include "../cps/cps.hpp"
+using namespace cps;
+
+int main() {
+    auto fileName{"visual_test.ps"};
+    fstream file(fileName, fstream::out);
+
+    file << START_FILE;
+
+    file << Spacer(4*INCH, 4*INCH).generate().str();
+
+    { // 3x3 grid
+        auto shapes = vector<Shape::Shape_ptr>();
+        shapes.push_back(make_unique<Triangle>(INCH));
+        shapes.push_back(make_unique<Square>(INCH));
+        shapes.push_back(make_unique<Polygon>(5, INCH));
+        shapes.push_back(make_unique<Polygon>(6, INCH));
+        shapes.push_back(make_unique<Polygon>(7, INCH));
+        shapes.push_back(make_unique<Polygon>(8, INCH));
+
+        HorizontalShapes column(move(shapes));
+
+        file << column.generate().str();
+    }
+
+
+    file << SHOWPAGE;
+
+    return 0;
+}
